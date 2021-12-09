bb3bf6b5600b80e3049db46ff3a4e4862cac2bab
Amrit Shukla <syeadmaazahmed@fmail.com>
2021-12-09 18:40:21 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calcApp';
+  answer = 0;
+  prime = '';
+  Add(first: string, second: string) {
+    var fele = parseInt(first);
+    var sele = parseInt(second);
+    this.answer = fele + sele;
+  }
+  Sub(first: string, second: string) {
+    var fele = parseInt(first);
+    var sele = parseInt(second);
+    this.answer = fele - sele;
+  }
+  Multiply(first: string, second: string) {
+    var fele = parseInt(first);
+    var sele = parseInt(second);
+    this.answer = fele * sele;
+  }
+  Divide(first: string, second: string) {
+    var fele = parseInt(first);
+    var sele = parseInt(second);
+    this.answer = fele / sele;
+  }
+  Fact(ele: string) {
+    var num = parseInt(ele);
+    var res = 1;
+    for (let i = 1; i <= num; i++) {
+      res = res * i;
+    }
+    this.answer = res;
+  }
+  Prime(ele: string) {
+    var num = parseInt(ele);
+    if (num == 1) 
+      this.prime = 'Not a prime number';
+    else {
+      var tmp = 0;
+      for (let i = 2; i < num; i++) {
+        if (num % i == 0) {
+          tmp += 1;
+        }
+      }
+      if (tmp > 0) 
+        this.prime = 'Not a prime number';
+      else 
+       this.prime = 'A prime number';
+    }
+  }
 }
